e8d8e5c0471b0d522e93ac1525313319bffe0211
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-18 14:24:15 +0800

    new notes

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define max(a,b) ((a) > (b) ? (a) : (b))
-#define min(a,b) ((a) < (b) ? (a) : (b))
+#define min(a,b) ((a) < (b) ? (a) : (b))    //宏定义max min函数
 int L, n;
 int x[1000000];
+int minT=0,maxT=0;
 int main(void)
 {
     int m;
-    int minT=0,maxT=0;
     scanf("%d\n%d", &L, &n);
     for (m = 0; m < n; m++)
     {
