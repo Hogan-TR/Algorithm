e7c816a7b72ce5980a3ff6f4c78a43c0012daabf
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-19 14:04:29 +0800

    递归 阶乘 简

--- 1.cpp
+++ 1.cpp
@@ -1,48 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <algorithm>
-using namespace std;
-bool binary_search(int x);
-int a[1000000] = {};
-int n,q;
+int fact(int n);
 int main(void)
 {
-    int i,j,k,h;
-    bool f=false;
-    scanf("%d\n%d", &n,&q);
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-    sort(a,a+n);
-    for(j=0;j<n;j++)
-    {
-        for(k=0;k<n;k++)
-        {
-            for(h=0;h<n;h++)
-            {
-                if(binary_search(q-a[j]-a[k]-a[h]))
-                f=true;
-            }
-        }
-    }
-    if(f)puts("Yes");
-    else puts("No");
+    int n;
+    printf("请输入你想阶乘的数:\n");
+    scanf("%d",&n);
+    printf("%d的阶乘为%d\n",n,fact(n));
     system("pause");
 }
-bool binary_search(int x)
+int fact(int n)
 {
-    int l = 0, m, r;
-    r=n;
-    while (r - l >= 1)    //反复操作直到存在范围为空
-    {
-        m = (r + l) / 2;
-        if (a[m] == x)
-            return true;  //找到x
-        else if (a[m] < x)
-            l = m + 1;
-        else
-            r = m;
-    }
-    return false;         //没找到x
+    if(n==0) return 1;                  // n 值亦可为 1
+    return n*fact(n-1);
 }
