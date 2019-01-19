f2d4f029104f31ffbf6da889f5008341ac1fa7ec
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-19 15:46:53 +0800

    栈

--- 1.cpp
+++ 1.cpp
@@ -1,25 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-int fact(int n);
-int fib(int n);
-int memo[1000000];
-int main(void)
+#include <stack>
+#include <cstdio>
+
+using namespace std;
+
+int main()
 {
-    int n;
-    printf("请输入一个数:\n");
-    scanf("%d",&n);
-    printf("%d的阶乘为%d\n",n,fact(n));
-    printf("%d的斐波那契数为%d\n",n,fib(n));
-    system("pause");
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    printf("%d\n",s.top());
+    s.pop();
+    printf("%d\n",s.top());
+    s.pop();
+    printf("%d\n",s.top());
+    s.pop();
+    getchar();
+    return 0;
 }
-int fact(int n)
-{
-    if(n==0) return 1;                  // n 值亦可为 1
-    return n*fact(n-1);
-} 
-int fib(int n)
-{
-    if(n<=1) return n;
-    if(memo[n]!=0) return memo[n];
-    return memo[n]=fib(n-1)+fib(n-2);
-}    
