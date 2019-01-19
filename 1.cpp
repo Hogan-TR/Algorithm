a87fb092ea7316381c00be971747b9278ae552fe
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-19 16:24:44 +0800

    队列 Queue

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,20 @@
-#include <stack>
+#include <queue>
 #include <cstdio>
 
 using namespace std;
 
 int main()
 {
-    stack<int> s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    printf("%d\n",s.top());
-    s.pop();
-    printf("%d\n",s.top());
-    s.pop();
-    printf("%d\n",s.top());
-    s.pop();
+    queue<int> que;
+    que.push(1);
+    que.push(2);
+    que.push(3);
+    printf("%d\n",que.front());
+    que.pop();
+    printf("%d\n",que.front());
+    que.pop();
+    printf("%d\n",que.front());
+    que.pop();
     getchar();
     return 0;
 }
