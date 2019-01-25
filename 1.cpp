ada87aba8f842914e7356526dedfb6d688179af3
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-25 20:03:55 +0800

    size of string

--- 1.cpp
+++ 1.cpp
@@ -1,34 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-using namespace std;
-int main()
+int main(void)
 {
-  long long n, m, x, w;
-  int i;
-  long long a[200020], b[200020];
-  scanf("%lld %lld", &n, &m);
-  w = 1;
-  memset(a, 0, sizeof(a));
-  memset(b, 0, sizeof(b));
-  for (i = 1; i <= n; i++)
-  {
-    scanf("%lld", &x);
-    a[i] = a[i - 1] + x;
-  }
-  for (i = 0; i < m; i++)
-  {
-    scanf("%lld", &b[i]);
-  }
-  for (i = 0; i < m; i++)
-  {
-    if (b[i] <= a[w])
-      printf("%lld %lld\n", w, b[i] - a[w - 1]);
-    else
-    {
-      w++;
-      i--;
-    }
-  }
-  return 0;
+  char a[]="Hello, World!";
+  printf("%d\n",sizeof(a));
+  system("pause");
 }
