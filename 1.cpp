d4765918fe1d90dd28e0665dd22af44c545966e2
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-25 22:09:40 +0800

    数组 前后倒置

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 int main(void)
 {
-  char a[]="Hello, World!";
-  printf("%d\n",sizeof(a));
+  int n,i,fal;
+  int a[110];
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n/2;i++)
+  {
+    fal=a[i];
+    a[i]=a[n-i-1];
+    a[n-i-1]=fal;
+  }
+  for(i=0;i<n;i++){
+    printf("%d ",a[i]);
+  }
   system("pause");
 }
