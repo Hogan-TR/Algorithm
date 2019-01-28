e3c57de76a828c5a072c62fdbd3b0a4766e9c1ca
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-28 20:54:56 +0800

    Time and Date 调用

--- 1.cpp
+++ 1.cpp
@@ -1,21 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(void)
 {
-  int n,i,fal;
-  int a[110];
-  scanf("%d",&n);
-  for(i=0;i<n;i++){
-    scanf("%d",&a[i]);
-  }
-  for(i=0;i<n/2;i++)
-  {
-    fal=a[i];
-    a[i]=a[n-i-1];
-    a[n-i-1]=fal;
-  }
-  for(i=0;i<n;i++){
-    printf("%d ",a[i]);
-  }
-  system("pause");
+	printf("Time: %s\n",__TIME__);
+	printf("Date: %s\n",__DATE__);
+	system("pause");
 }
