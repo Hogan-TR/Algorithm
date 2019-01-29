d43043378a9e27192626650d9c8dbf88ae4cc4aa
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-29 14:34:54 +0800

    指针_内存地址

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#define square(x) ((x)*(x))
-#define square_1(x) (x*x)
-        //参数括号很重要
-int main(void)     
+#include <stdlib.h>
+int main(void)
 {
-	printf("square 5+4 is %d\n",square(5+4));
-	printf("square_1 5+4 is %d\n",square_1(5+4));
-	return 0;
+	int var1;
+	char var2[10];
+	printf("var1 变量的地址：%p\n",&var1);
+	printf("var2 变量的地址：%p\n",&var2);
+	system("pause");
 }
-
-//square   等价于   （5+4）*（5+4）=81
-//square_1 等价于   5+4*5+4=29
