ce2de2d01ffc7b1095e6dccc1c459796a51855b6
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-30 20:24:10 +0800

    指针_实例3-反例

--- 1.cpp
+++ 1.cpp
@@ -5,13 +5,15 @@ int *test()
 	int i=5,*pi=&i;
 	return pi;
 }
+void test2()
+{
+	int j=3;
+}
 int main(void)
 {
-	int *pj;
-	pj=test();
-	printf("%d\n",*pj);
+	int *pr;
+	pr=test();
+	test2();
+	printf("%d\n",*pr);
 	system("pause");
 }
-
-//非法内存访问错误：仅指访问了无权访问的内存(不能访问的内存)
-//当储存单元不再属于程序所拥有时，即使在程序中可以通过指针访问储存单元，也不应该再使用该储存单元了，以免出现不可预知的错误
