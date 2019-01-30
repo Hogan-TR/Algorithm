20a2d0c79561e74d26016811f239a20e03aa51fa
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-30 21:27:15 +0800

    指针_Fault-Test

--- 1.cpp
+++ 1.cpp
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-int *test()
-{
-	int i=5,*pi=&i;
-	return pi;
-}
-void test2()
-{
-	int j=3;
-}
 int main(void)
 {
-	int *pr;
-	pr=test();
-	test2();
-	printf("%d\n",*pr);
+	int i=3,*p,*q;
+	p=&i;
+ 	// *q=i;                           //此赋值方式错误
+	printf("%d %p\n",*p,p);
+	// printf("%d %p\n",*q,q);
 	system("pause");
 }
