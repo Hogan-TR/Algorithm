ca7e39bdf21f20768fc29de4984914c7cab4b6d0
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-01-30 16:45:49 +0800

    指针_实例2

--- 1.cpp
+++ 1.cpp
@@ -1,25 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-void swap(int *px,int *py)
+void add(int x,int y,int *pr)
 {
-	int temp;
-	temp=*px;
-	*px=*py;
-	*py=temp;
+	*pr=x+y;
 }
 int main(void)
 {
-	int i=2,j=3;
-	printf("%d %d\n",i,j);
-	swap(&i,&j);               //swap(i,j)  不可这样调用
-	printf("%d %d\n",i,j);
+	int i=23,j=32,k=0;
+	add(i,j,&k);
+	printf("%d+%d=%d\n",i,j,k);
 	system("pause");
 }
-
-// 函数调用为传值调用
-// swap(2,3)
-// 实参赋值给形参
-// px=2,py=3
-// 显然，整型给指针赋值是错误的
-// 既然，形参为指针，则必须给他赋值为地址
-// 所以，调用时为  swap(&i,&j)
