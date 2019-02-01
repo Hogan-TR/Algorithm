dc76a3aae1567f573851a3e217421af188e0df41
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-01 15:52:30 +0800

    指针_字符串常量

--- 1.cpp
+++ 1.cpp
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-void swap(int a[])   //数组类型为形参时会退化成相应的指针类型,即为 void swap(int *a)
+void print(char *p)
 {
-	int temp;
-	temp=a[0];	//*(a+0) 即*a
-	a[0]=a[1];	//*(a+1)
-	a[1]=temp;
+	while(*p!='\0')
+	{
+		putchar(*p++);
+	}
 }
 int main(void)
 {
-	int b[2]={2,3};
-	swap(b);
-	printf("%d %d\n",b[0],b[1]);
+	char ch[]="Hello";
+	print(ch);
 	system("pause");
 }
