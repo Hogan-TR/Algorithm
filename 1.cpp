130e46ec620a61d827087d7537da611f8f318589
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-01 09:47:35 +0800

    指针_实例6-数组的退化

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-void swap(int *a)
+void swap(int a[])   //数组类型为形参时会退化成相应的指针类型,即为 void swap(int *a)
 {
 	int temp;
-	temp=a[0];
-	a[0]=a[1];
+	temp=a[0];	//*(a+0) 即*a
+	a[0]=a[1];	//*(a+1)
 	a[1]=temp;
 }
 int main(void)
