a1601351e53915605ab4174467f7ac18905d4a17
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-01 17:10:14 +0800

    指针_实例9
    一维指针数组作为函数参数

--- 1.cpp
+++ 1.cpp
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+void printA(int n,char **pp) //char *pp[]
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		puts(*pp++);
+	}
+}
 int main(void)
 {
-	char *a[3]={"C","C++","Java"};
-	char *p,**pp;
-	puts(a[1]);					//C++
-	printf("%c\n",*(a[2]+2));   //v
-	pp=a;
-	puts(*(pp+1));				//C++
-	printf("%c\n",*(*(pp+1)+1));//+
-	puts(*(pp+1)+1);			//++
-	p=a[1];
-	puts(p+1);					//++
-	pp=&p;
-	puts(*pp);					//C++
-	// puts(*(pp-1));			野指针
-	pp=&a[1];
-	puts(*(pp-1));
+	char *a[]={"C","C++","Java","C#"};
+	printA(sizeof(a)/sizeof(*a),a);
 	system("pause");
 }
