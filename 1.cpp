c5e4b825ec19106144ebde2ff37eda3f18c7c198
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-01 16:58:47 +0800

    指针_实例8
    指向存储单元类型为字符型指针的指针

--- 1.cpp
+++ 1.cpp
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 int main(void)
 {
-	char ch[]="Hello!";
-	char *str;
-	char *str1="Hello!";	 //字符型字面量，通常以字符数组的形式存储于内存中一个“只读”的特殊内存区域，而且整个字符常量表现为与首字符相关的存储单元的首地址
-	str=ch;		        //str 指向 ch[0]
-	puts(str);          //puts(ch)
-	*(str+1)='i';       //*(ch+1)  ch[1]
-	str[2]='!';			//*(str+2)  *(ch+2)  ch[2]
-	*(str+3)='\0';
-	puts(ch);      
-	puts(str1);
-	// *str1='A';       //只读的访问区域，内存访问错误
+	char *a[3]={"C","C++","Java"};
+	char *p,**pp;
+	puts(a[1]);					//C++
+	printf("%c\n",*(a[2]+2));   //v
+	pp=a;
+	puts(*(pp+1));				//C++
+	printf("%c\n",*(*(pp+1)+1));//+
+	puts(*(pp+1)+1);			//++
+	p=a[1];
+	puts(p+1);					//++
+	pp=&p;
+	puts(*pp);					//C++
+	// puts(*(pp-1));			野指针
+	pp=&a[1];
+	puts(*(pp-1));
 	system("pause");
 }
