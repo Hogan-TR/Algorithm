7f94ad8a485413a4bd83e907f39717037f506193
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-01 09:40:35 +0800

    指针_实例6

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 5
-int a[N]={23,32,25,52,21};
+void swap(int *a)
+{
+	int temp;
+	temp=a[0];
+	a[0]=a[1];
+	a[1]=temp;
+}
 int main(void)
 {
-	int i,j,min,temp,*pi;
-	pi=a;
-	for(i=0;i<N-1;i++)
-	{
-		min=i;
-		for(j=i+1;j<N;j++)
-		{
-			if(*(pi+min)>*(pi+j))  //pi[min]  a[min]  *(a+min)  等价
-			min=j;
-		}
-		temp=pi[i];
-		pi[i]=pi[min];
-		pi[min]=temp;
-	}
-	for(i=0;i<N;i++)
-	{
-		printf("%3d",*pi++);
-	}
+	int b[2]={2,3};
+	swap(b);
+	printf("%d %d\n",b[0],b[1]);
 	system("pause");
 }
-//排序
