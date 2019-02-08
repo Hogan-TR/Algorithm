382c7f28bf641a7e4b48f55f0edf2fd4fd47caa9
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-08 21:08:40 +0800

    ICPC-2.1

--- 1.cpp
+++ 1.cpp
@@ -1,35 +1,10 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
 int main()
 {
-	int n, i, ii;
-	char s[100];
-	char pair[99][2];
-	char result[2];
-	int times;
-	int resulttimes = 0;
-	cin >> n >> s;
-	for (i = 0; i < n - 1; i++)
+	int m, n;
+	while (~scanf("%d%d", &m, &n))  //scanf("%d%d", &m, &n)!=EOF  亦可
 	{
-		pair[i][0] = s[i];
-		pair[i][1] = s[i + 1];
+		printf("%d\n", m + n);
 	}
-	for (i = 0; i < n - 1; i++)
-	{
-		times = 0;
-		for (ii = 0; ii < n - 1; ii++)
-		{
-			if (pair[i][0] == pair[ii][0])
-				if (pair[i][1] == pair[ii][1])
-					times++;
-		}
-		if (times > resulttimes)
-		{
-			resulttimes = times;
-			result[0] = pair[i][0];
-			result[1] = pair[i][1];
-		}
-	}
-	cout << result[0] << result[1] << "\n";
 	return 0;
 }
