f26913b1eafad68d342b2e0fccfa1a4d689212d5
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-08 23:49:12 +0800

    ICPC-打表

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
+using namespace std;
 int main()
 {
-    long long a;
-    scanf("%lld",&a);
-    switch(a%4)
-    {
-        case 0: case 3: {printf("0"); break;}
-        case 1: case 2: {printf("1"); break;}
-    }
-    return 0;
+	for (int i = 1; i <= 100; i++) {
+		int res = 1;
+		for (int j = 1; j <= i; j++) {
+			res *= i;
+			res %= 10;
+		}
+		printf("%d:%d\n", i, res);
+	}
+	return 0;
 }
