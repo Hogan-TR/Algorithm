11f302578e20bfb08246a71b42ec03f92d262b6d
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-08 12:03:32 +0800

    ICPC-1.3

--- 1.cpp
+++ 1.cpp
@@ -1,32 +1,38 @@
 #include <stdio.h>
+#include <math.h>
 using namespace std;
+int gcd(int x, int y) {//辗转相除法求最大公约数，x>=y;
+	if (x < y) {
+		int tmp = x;
+		x = y;
+		y = tmp;
+	}
+	if (y == 0)
+		return x;
+	return gcd(y, x % y);
+}
 int main(void)
 {
-	int n,check=0;
-	char s[20005];
-	scanf("%d", &n); getchar();
-	for (int i = 0; i < n; i++) {
-		scanf("%c", &s[i]);
-		getchar();
-	}
-	int a = 0, b = n - 1;  //用a，b很巧妙
-	while (a <= b) {
-		//将从左起和从右起的字符串比较
-		bool left = false;
-		for (int i = 0; a + i <= b; i++) {
-			if (s[a + i] < s[b - i]) {
-				left = true;
-				break;
-			}
-			else if (s[a + i] > s[b - i]) {
-				left = false;
-				break;
-			}
-		}  //此中用循环__目的__处理前后相同的情况
-		if (check == 80) { printf("\n"); check = 0; }
-		if (left) putchar(s[a++]);	//妙
-		else putchar(s[b--]);
-		check++;
+	int a, b, c, d, _gcd, _lcm, numerator, _a, _c, _gcd1;
+	char o;
+	while (scanf("%d/%d%c%d/%d", &a, &b, &o, &c, &d) != EOF) {
+		_gcd = gcd(b, d);		//最大公约数
+		_lcm = b / _gcd * d;	//两个分母的最小公倍数
+		_a = a * d / _gcd;		//通分后一分子
+		_c = c * b / _gcd;		//通分后一分子
+		if (o == '+')
+			numerator = _a + _c;
+		else
+			numerator = _a - _c;
+		if (numerator == 0)		//若分子值为0.则直接输出
+			printf("%d\n", numerator);
+		else {
+			_gcd1 = gcd(abs(numerator), _lcm);	//最大公约数和分母相同，说明分子和分母相同,最后值为整数
+			if (_gcd1 == _lcm)
+				printf("%d\n", numerator / _gcd1);
+			else
+				printf("%d/%d\n", numerator / _gcd1, _lcm / _gcd1);
+		}
 	}
 	return 0;
 }
