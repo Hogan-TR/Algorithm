9baaca497c40c450e48a02e867cf6d499d907050
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-09 23:22:51 +0800

    ICPC-2.5

--- 1.cpp
+++ 1.cpp
@@ -1,26 +1,70 @@
+			/*找规律*/
+
+// #include <cstdio>
+// #include <cstring>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// char s1[200005];
+// char s2[200005];
+// unsigned a[5];
+// int main()
+// {
+// 	int n;
+// 	scanf("%d", &n);
+// 	scanf("%s%s", s1, s2);
+// 	for (int i = 0; i < n; i++) {
+// 		if (s1[i] == '0') {
+// 			if (s2[i] == '0') a[1]++;
+// 			else a[2]++;
+// 		}
+// 		else {
+// 			if (s2[i] == '1')  a[3]++;
+// 			else  a[4]++;
+// 		}
+// 	}
+// 	long long ans = 0;
+// 	ans = a[2] * a[4] + 1ll * a[1] * a[3] + 1ll * a[1] * a[4];
+// 	printf("%lld\n", ans);
+// 	return 0;
+// }
+
+
 #include<iostream>
-#include<stdio.h>
+#include<cstdio>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
 using namespace std;
-/**
-  快速幂算法
-*/
-long long Calculation(long long a, long long b, long long c) {
-	int ans = 1;
-	a = a % c;
-	while (b > 0) {
-		if (b % 2 == 1)
-			ans = (ans * a) % c;
-		b = b / 2;
-		a = (a * a) % c;
-	}
-	return ans;
-}
-int main() {
-	int n, N;
+const int maxn = 100005;
+int a[maxn], b[maxn];
+char ch1[maxn], ch2[maxn];
+int sum[5];
+int main()
+{
+	int n;
 	cin >> n;
-	while (n--) {
-		cin >> N;
-		printf("%d\n", Calculation(N, N, 10));
+	scanf("%s%s", &ch1, &ch2);
+	for (int i = 0; i < n; i++) {
+		a[i] = ch1[i] - '0';
+		sum[a[i]]++;
 	}
+	for (int i = 0; i < n; i++) {
+		b[i] = ch2[i] - '0';
+	}
+	long long ans = 0;
+	for (int i = 0; i < n; i++) {
+		if (b[i] == 0) {
+			if (a[i] = 1) {
+				ans += sum[0];
+				sum[1]--;
+			}
+			else {
+				ans += sum[1];
+				sum[0]--;
+			}
+		}
+	}
+	cout << ans << endl;
 	return 0;
 }
