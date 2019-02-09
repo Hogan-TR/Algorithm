4404b0c7ab2c10e8e283b7b9496e6cf1765ad235
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-10 00:20:47 +0800

    ICPC-2.6

--- 1.cpp
+++ 1.cpp
@@ -1,70 +1,32 @@
-			/*找规律*/
-
-// #include <cstdio>
-// #include <cstring>
-// #include <iostream>
-// #include <algorithm>
-// using namespace std;
-// char s1[200005];
-// char s2[200005];
-// unsigned a[5];
-// int main()
-// {
-// 	int n;
-// 	scanf("%d", &n);
-// 	scanf("%s%s", s1, s2);
-// 	for (int i = 0; i < n; i++) {
-// 		if (s1[i] == '0') {
-// 			if (s2[i] == '0') a[1]++;
-// 			else a[2]++;
-// 		}
-// 		else {
-// 			if (s2[i] == '1')  a[3]++;
-// 			else  a[4]++;
-// 		}
-// 	}
-// 	long long ans = 0;
-// 	ans = a[2] * a[4] + 1ll * a[1] * a[3] + 1ll * a[1] * a[4];
-// 	printf("%lld\n", ans);
-// 	return 0;
-// }
-
-
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<string.h>
-#include<math.h>
-using namespace std;
-const int maxn = 100005;
-int a[maxn], b[maxn];
-char ch1[maxn], ch2[maxn];
-int sum[5];
+#include <stdio.h>
+int a[5001], cnt[5001], color[5001];
 int main()
 {
-	int n;
-	cin >> n;
-	scanf("%s%s", &ch1, &ch2);
-	for (int i = 0; i < n; i++) {
-		a[i] = ch1[i] - '0';
-		sum[a[i]]++;
+	int n, k, t = 0;
+	scanf("%d%d", &n, &k);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		cnt[a[i]]++;	//计数——一个数字出现次数
 	}
-	for (int i = 0; i < n; i++) {
-		b[i] = ch2[i] - '0';
-	}
-	long long ans = 0;
-	for (int i = 0; i < n; i++) {
-		if (b[i] == 0) {
-			if (a[i] = 1) {
-				ans += sum[0];
-				sum[1]--;
-			}
-			else {
-				ans += sum[1];
-				sum[0]--;
+	for (int i = 1; i <= 5000; i++) {
+		if (cnt[i] > k) {
+			printf("NO\n");
+			return 0;
+		}
+		else {
+			for (int j = 1; j <= n; j++) {
+				if (a[j] == i)
+				{
+					color[j] = ++t;
+					t %= k;		//超过了颜色数就再次从1开始
+				}
 			}
 		}
 	}
-	cout << ans << endl;
+	printf("YES\n");
+	for (int i = 1; i <= n; i++) {
+		if (i != 1) printf(" ");
+		printf("%d", color[i]);
+	}
 	return 0;
 }
