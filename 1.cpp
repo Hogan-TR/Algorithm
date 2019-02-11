79bd9d070d042188dd7613ab41f7f1c3fbce4a38
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-11 16:26:46 +0800

    坑——'-'

--- 1.cpp
+++ 1.cpp
@@ -3,22 +3,24 @@
 using namespace std;
 int main()
 {
-	int n;
-	bool m = false;
-	scanf("%d", &n);
-	if (n % 3 == 0) {
-		printf("3 ");
-		m = true;
+	double a, b, c;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	double dec = b * b - 4 * a * c;
+	if (dec == 0) {
+		printf("x1=x2=%.5f", (-b + sqrt(b * b - 4 * a * c)) / (2 * a));
 	}
-	if (n % 5 == 0) {
-		printf("5 ");
-		m = true;
+	else if (dec > 0) {
+		double x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a), x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+		double temp;
+		if (x1 < x2) {
+			temp = x1;
+			x1 = x2;
+			x2 = temp;
+		}
+		printf("x1=%.5f;x2=%.5f", x1, x2);
 	}
-	if (n % 7 == 0) {
-		printf("7 ");
-		m = true;
+	else {									        //这正下方有点坑
+		printf("x1=%.5f+%.5fi;x2=%.5f-%.5fi", 0-b / (2 * a), sqrt(4 * a * c - b * b) / (2 * a), 0-b / (2 * a), sqrt(4 * a * c - b * b) / (2 * a));
 	}
-	if (!m)
-		printf("n");
 	return 0;
 }
