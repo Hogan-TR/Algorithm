105e941f1d809f23d8e420b09df561011276d5aa
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-02-17 09:41:16 +0800

    MOOC-POINTER

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 int main()
+// {
+// 	int a[] = {5, 15, 34, 54, 14, 2, 52, 72};
+// 	int *p = &a[5];
+// 	printf("%d",p[-2]);
+// 	system("pause");
+// }
+
+// {
+// 	int a[] = {0};
+// 	int *p = a;
+// 	// if(p == a[0])
+// 	// printf("1");
+// 	if(p == &a[0])
+// 	printf("2");
+// 	if(*p == a[0])
+// 	printf("3");
+// 	if(p[0] == a[0])
+// 	printf("4");
+// 	system("pause");
+// }
 {
-	for (int i = 49; i <= 819; i++) {
-		int res1 = 0, res2 = 0;
-		int u, t;
-		int a[3], b[3];
-		u = t = i;
-		for(int r = 0; r < 3; r++) {		//十进制转七进制
-			int temp;
-			temp = u % 7;
-			u /= 7;
-			a[r] = temp;
-		}
-		for (int r = 0; r < 3; r++) {		//十转九
-			int temp;
-			temp = t % 9;
-			t /= 9;
-			b[r] = temp;
-		}
-		if (a[0] == b[2] && a[2] == b[0])
-		{
-			printf("%d\n", i);
-			for (int y = 0; y < 3; y++) {
-				printf("%d", b[y]);
-			}
-			printf("\n");
-			for (int y = 0; y < 3; y++) {
-				printf("%d", a[y]);
-			}
-			printf("\n");
-		}
-	}
-	return 0;
+	int a[] = {5, 15, 34, 54, 14, 2, 52, 72};
+	int *p = &a[1];
+	printf("%d",p[2]);
+	system("pause");
 }
-
-
-/*一个十进制自然数,它的七进制与九进制表示都是三位数，且七进制与九进制的三位数码表示顺序正好相反。编程求此自然数,并输出显示*/
-/*十进制转其他进制：除以其他，取余数，倒置*/
