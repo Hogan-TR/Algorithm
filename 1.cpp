22d9465e1e40a3da04321e0d12f91eaf8f06e37b
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-03-06 21:14:22 +0800

    冒泡排序_2: 结构体的使用

--- 1.cpp
+++ 1.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
 using namespace std;
-void pao(int a[], int n);
+void pao(int n);
+struct student
+{
+    char name[21];
+    int score;
+};
+struct student a[100], t; //t为临时变量
 int main()
 {
-    int n, a[n];
+    int n;
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        cin >> a[i];
+        scanf("%s %d", a[i].name, &a[i].score);
     }
-    pao(a, n);
+    pao(n);
     return 0;
 }
-void pao(int a[], int n)
+void pao(int n)
 {
-    int temp;
     // n 个数，则跑 n-1 次
     for (int i = 1; i <= n - 1; i++)
     {
         for (int j = 1; j <= n - i; j++)
         {
-            if (a[j] > a[j + 1])
+            if (a[j].score < a[j + 1].score)
             {
-                temp = a[j];
+                t = a[j];
                 a[j] = a[j + 1];
-                a[j + 1] = temp;
+                a[j + 1] = t;
             }
         }
     }
     for (int i = 1; i <= n; i++)
     {
-        printf("%d ", a[i]);
-    }    
+        cout << a[i].name << endl;
+    }
 }
