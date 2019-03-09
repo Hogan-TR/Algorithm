6397adaaa9374bae86444dbc99fa5647b3dd587b
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-03-09 18:41:24 +0800

    快速排序
    同时对工作台环境Reconfigure

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,60 @@
+/*快速排序*/
 #include <iostream>
 using namespace std;
-void pao(int n);
-struct student
-{
-    char name[21];
-    int score;
-};
-struct student a[100], t; //t为临时变量
+void quicksort(int left, int right);
+int n, a[600];
 int main()
 {
-    int n;
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        scanf("%s %d", a[i].name, &a[i].score);
+        cin >> a[i];
     }
-    pao(n);
-    return 0;
-}
-void pao(int n)
-{
-    // n 个数，则跑 n-1 次
-    for (int i = 1; i <= n - 1; i++)
-    {
-        for (int j = 1; j <= n - i; j++)
-        {
-            if (a[j].score < a[j + 1].score)
-            {
-                t = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = t;
-            }
-        }
-    }
+    quicksort(1, n);
     for (int i = 1; i <= n; i++)
     {
-        cout << a[i].name << endl;
+        cout << a[i];
+        cout << " ";
     }
+    return 0;
+}
+void quicksort(int left, int right)
+{
+    int i, j, temp, t;
+
+    //快排用递归的出口
+    if (left > right)
+        return;
+
+    //temp为基准数
+    temp = a[left];
+    i = left;
+    j = right;
+
+    //整个while将基准数两端数归位
+    while (i != j)
+    {
+
+        // 顺序很重要，先从右往左找
+        while (a[j] >= temp && i < j)
+            j--;
+        while (a[i] <= temp && i < j)
+            i++;
+
+        //交换两个数在数组中的位置
+        if (i < j) //当i，j未相遇
+        {
+            t = a[j];
+            a[j] = a[i];
+            a[i] = t;
+        }
+    }
+
+    //将基准数归位
+    a[left] = a[i];
+    a[i] = temp;
+
+    //递归
+    quicksort(left, i - 1);
+    quicksort(i + 1, right);
 }
