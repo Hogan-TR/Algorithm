72ec21b83e7dbd676f28f60505787c28a52d483b
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-03-20 17:48:07 +0800

    数组模拟链表

--- 1.cpp
+++ 1.cpp
@@ -1,54 +1,40 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
-//这里创建一个结构体用来表示链表的结点类型
-struct node
-{
-    int data;
-    struct node *next;
-};
-
 int main()
 {
-    struct node *head, *p, *q, *t;
-    int i, n, a;
+    int data[101], right[101];
+    int i, n, t, len;
     cin >> n;
-    head = NULL; //头指针初始为空
+    for (i = 1; i <= n; i++)
+        cin >> data[i];
+    len = n;
     for (i = 1; i <= n; i++)
     {
-        cin >> a;
-        //动态申请一个空间，并用临时指针p指向此节点
-        p = (struct node *)malloc(sizeof(struct node));
-        p->data = a;
-        //将当前节点的后继指针指向空 -> 当前节点的下一个节点为空
-        p->next = NULL;
-        if (head == NULL)
-            head = p;
+        if (i != n)
+            right[i] = i + 1;
         else
-            //若不是第一个创建的节点，则将上一个节点的后继指针指向当前节点
-            q->next = p;
-        q = p;
+            right[i] = 0;
+    }
+    len++;
+    cin >> data[len];
+
+    t = 1;
+    while (t != 0)
+    {
+        if (data[right[t]] > data[len])
+        {
+            right[len] = right[t];
+            right[t] = len;
+            break;
+        }
+        t = right[t];
     }
 
-    cin >> a; //读入待插入的数
-    t = head;
-    while (t != NULL) //当没有到达链表尾部时循环
+    t = 1;
+    while (t != 0)
     {
-        if (t->next->data > a)
-        {
-            p = (struct node *)malloc(sizeof(struct node));
-            p->data = a;
-            p->next = t->next;
-            t->next = p;
-            break; //插入完毕，推出循环
-        }
-        t = t->next; //继续下一个节点
-    }
-    t = head;
-    while (t != NULL)
-    {
-        printf("%d ", t->data);
-        t = t->next;
+        printf("%d ", data[t]);
+        t = right[t];
     }
     return 0;
 }
