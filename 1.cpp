cdd43305eb100a65d7cd060b0c3770dfd3c6103c
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-03-20 23:07:21 +0800

    dfs-1

--- 1.cpp
+++ 1.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 using namespace std;
-int fun(int x);
+void dfs(int step);
+int a[10], book[10], n;
+//C语言的全局变量，未赋值前默认为0
 int main()
 {
-    int a, b, c, m, i, sum = 0, t = 0;
-    scanf("%d", &m);
-    for (a = 0; a <= 1111; a++)
-    {
-        for (b = 0; b <= 1111; b++)
-        {
-            c = a + b;
-            if (fun(a) + fun(b) + fun(c) == m - 4)
-            {
-                t++;
-                printf("ex%d: %d+%d=%d\n", t, a, b, c);
-                sum++;
-            }
-        }
-    }
-    printf("Total: %d", sum);
+    scanf("%d", &n);
+    dfs(1);
     return 0;
 }
-int fun(int x)
+void dfs(int step)
 {
-    int num = 0;
-    int f[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
-    while (x / 10 != 0)
+    int i;
+    //到 n+1 盒子面前
+    if (step == n + 1)
     {
-        num += f[x % 10];
-        x /= 10;
+        for (i = 1; i <= n; i++)
+            printf("%d", a[i]);
+        printf("\n");
+        //返回之前的一步 ->最近一次调用dfs函数的地方
+        return;
     }
-    num += f[x];
-    return num;
+    //按照1、2、3...n的顺序尝试
+    for (i = 1; i <= n; i++)
+    {
+        //判断扑克牌i是否还在手中
+        if (book[i] == 0)
+        {
+            a[step] = i;
+            book[i] = 1;
+            //到下一个盒子面前
+            dfs(step + 1);
+            //将刚才尝试的扑克牌收回，才能进行下一次尝试
+            book[i] = 0;
+        }
+    }
+    return;
 }
