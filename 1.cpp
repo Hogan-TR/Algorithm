ff24a166984f9949365e7dc09c781933f62307c2
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-03-20 23:28:47 +0800

    dfs-2
    相加和 1~9个不同数字

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 void dfs(int step);
-int a[10], book[10], n;
+int a[10], book[10], n, times = 0;
 //C语言的全局变量，未赋值前默认为0
 int main()
 {
@@ -11,13 +11,12 @@ int main()
 }
 void dfs(int step)
 {
-    int i;
+    int i, t = 0;
     //到 n+1 盒子面前
-    if (step == n + 1)
+    if (step == n + 1 && a[1] * 100 + a[2] * 10 + a[3] + a[4] * 100 + a[5] * 10 + a[6] == a[7] * 100 + a[8] * 10 + a[9])
     {
-        for (i = 1; i <= n; i++)
-            printf("%d", a[i]);
-        printf("\n");
+        times++;
+        printf("Ex-%d: %d%d%d + %d%d%d = %d%d%d\n", times, a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9]);
         //返回之前的一步 ->最近一次调用dfs函数的地方
         return;
     }
