be96009c56f9a97d7ad291277ab6a0800df9cdb0
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-09-07 20:43:57 +0800

    递归-最大值

--- 1.cpp
+++ 1.cpp
@@ -1,48 +1,15 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
-int x, y, p, q,mi=9999999;
-int m, n;
-int a[1000][1000];
-int book[1000][1000];
-void dfs(int x, int y, int step);
-int main()
-{
-    //行 列
-    cin >> m >> n;
-    for (int i = 1; i <= m; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
-    scanf("%d%d%d%d", &x, &y, &p, &q);
-    book[x][y]=1;
-    dfs(x,y,0);
-    printf("%d",mi);
-    return 0;
+int func(int arr[], int n) {
+	if(n == 0)
+		return arr[0];
+	if(func(arr, n-1) > arr[n])
+		return func(arr, n-1);
+	else
+		return arr[n];
 }
-void dfs(int x, int y, int step)
-{
-    int e[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    int tx, ty;
-    if (x == p && y == q)
-    {
-        if (step < mi)
-            mi = step;
-        return;
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        tx = x + e[i][0];
-        ty = y + e[i][1];
-
-        if (tx < 1 || tx > m || ty < 1 || ty > n)
-            continue;
-        if (a[tx][ty] == 0 && book[tx][ty] == 0)
-        {
-            book[tx][ty] = 1;
-            dfs(tx, ty, step + 1);
-            book[tx][ty] = 0;
-        }
-    }
-    return;
+int main() {
+	int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	cout << func(arr, 8) << endl;
+	return 0;
 }
